5763a6d76b94d6df0429585e56bdd8079e766455
Appiah Beatrice Naa- Meryea <miss-meryea@Appiahs-MacBook-Air.local>
2024-10-19 10:40:27 +0000

    question1

---
+++ Bash_Logic_Operations/greet_user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script is to take 2 positional arguments and display a greeting
+
+echo Please enter your First Name
+read firstname
+echo Please enter your Last Name
+read lastname
+echo "Hello, $firstname $lastname! 
+     Welcome to Bash Scripting. "
