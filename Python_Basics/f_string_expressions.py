2c901973239e547887a402a47e5dea95eab48302
Appiah Beatrice Naa- Meryea <miss-meryea@Appiahs-MacBook-Air.local>
2024-10-23 07:19:09 +0000

    Question 4

---
+++ Python_Basics/f_string_expressions.py
@@ -0,0 +1,14 @@
+#!/Library/Frameworks/Python.framework/Versions/3.13/bin/python3
+# This script is to using f' string to print a message showing the sum, difference and product of some variables
+
+a=10
+b=20
+
+Sum=f'{a+b}'
+print(Sum)
+
+Difference=f'{a-b}'
+print(Difference)
+
+Product=f'{a*b}'
+print(Product)
