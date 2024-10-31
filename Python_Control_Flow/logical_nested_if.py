f19a049cbbbe46eb556e94fcb178da38ccbd63a7
Appiah Beatrice Naa- Meryea <miss-meryea@Appiahs-MacBook-Air.local>
2024-10-31 16:50:20 +0000

    Question 7{iii}

--- Python_Control_Flow/logical_nested_if.py
+++ Python_Control_Flow/logical_nested_if.py
@@ -11,3 +11,17 @@ if is_student or not is_employed:
       print("You are a student.")
 if not is_student and is_employed:
      print("You are employed but not a student.")
+
+
+# {iii} Nested Loop
+message = 'Please enter your age...  '
+age = int(input(message))
+message += "\nDo you have a driver's license? Yes or No. "
+response = (input(message))
+
+if age >= 18 and response == 'Yes':
+      print("You are allowed to drive.")
+else:
+      print("You need a driver's license to drive.")
+if age <= 18:
+      print('You are not old enough to drive')
